53dbc86f569255ec4abf33fff1dc92cb38a9df9f
Mateo <matugrisotto@gmail.com>
2025-05-28 00:52:40 -0300

    Sprint 8 en proceso

---
+++ dashboard-react/src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Dashboard = () => {
+    return (
+        <div>
+            <h1>AutoNet</h1>
+            <p>contenido</p>
+        </div>
+    )
+}
---
+++ dashboard-react/src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const SideBar = () => {
+    return <div>
+        <img src="/" alt="logo"/>
+        <ul>
+            <li><a href="Catálogo"></a></li>
+            <li><a href="Ultima publicación"></a></li>
+            <li><a href="Categorías"></a></li>
+            <li><a href="Total de publicaciones"></a></li>
+        </ul>
+    </div>
+};
